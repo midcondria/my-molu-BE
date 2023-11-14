12b956bf44ea219077fed68be8cedc5ec4985863
midcondria <hyukkind@naver.com>
2023-11-14 22:21:51 +0900

    init : 프로젝트 생성
    셀레니움 의존성 추가까지만 한 완전 초기 상태

---
+++ src/main/java/com/mymolu/MyMoluApplication.java
@@ -0,0 +1,13 @@
+package com.mymolu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymolu/MyMoluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymolu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
