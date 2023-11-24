ec35347ec3f951bfca26567b0fb2d0f63e77121b
midcondria <hyukkind@naver.com>
2023-11-24 23:06:46 +0900

    fix : 몰루 픽시브 일러스트 크롤링 기능 v1 버그 픽스
    CrawlingService
     - 크롬 드라이버를 빈으로 사용할 때 문제점 발견
       - 빈으로 쓰는건 스프링 구동 시 한 번 초기화 시켜놓고 한 인스턴스를 계속 돌려 쓰는것임
       - 따라서 기존 방식대로라면 크롤링 한 번 하고 크롬 창을 닫으면 다시 쓸 수 없음
     - 빈으로 등록해서 사용하는게 아닌 config 클래스의 스태틱 메서드로 사용
       - SelenimuConfig 를 빈으로 등록하여 사용할 수도 있을듯
       - 추후 필요 시 수정 예정

--- src/main/java/com/mymolu/domains/crawling/application/CrawlingService.java
+++ src/main/java/com/mymolu/domains/crawling/application/CrawlingService.java
@@ -1,13 +1,12 @@
 package com.mymolu.domains.crawling.application;
 
 
+import com.mymolu.global.config.SeleniumConfig;
 import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -16,14 +15,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class CrawlingService {
 
-    private final ChromeDriver driver;
-
     public static final String URL_ILLUSTRATION = "https://www.pixiv.net/tags/%E3%83%96%E3%83%AB%E3%83%BC%E3%82%A2%E3%83%BC%E3%82%AB%E3%82%A4%E3%83%96/illustrations";
 
     // 짤 크롤링
     public static final String IMAGE_BOX_ILLUSTRATION = "#root > div.charcoal-token > div > div:nth-child(4) > div > div > div.sc-15n9ncy-0.jORshO > section > div.sc-l7cibp-0.juyBTC > div:nth-child(1) > ul > li";
 
-    public void getImages() throws InterruptedException {
+    public List<String> getImages() throws InterruptedException {
+        ChromeDriver driver = SeleniumConfig.chromeDriver();
         driver.get(URL_ILLUSTRATION);
 
         List<String> images = new ArrayList<>();
@@ -34,5 +32,6 @@ public class CrawlingService {
             System.out.println(imageUrl);
         }
         driver.quit();
+        return images;
     }
 }
