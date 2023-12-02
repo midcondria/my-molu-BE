e191010d2025179d121233087bec78eca6cb8dad
midcondria <hyukkind@naver.com>
2023-12-03 00:13:45 +0900

    refactor : getImages() 메서드 리팩토링
    CrawlingService
     - 메서드 명을 getImages() -> downloadImages() 로 변경
       - 메서드에서 자동 스크롤 부분 메서드 추출
       - 이미지 저장 메서드를 스트림으로 코드 수를 줄임

--- src/main/java/com/mymolu/domains/crawling/application/CrawlingService.java
+++ src/main/java/com/mymolu/domains/crawling/application/CrawlingService.java
@@ -25,32 +25,36 @@ public class CrawlingService {
     // 짤 크롤링
     private static final String URL_ILLUSTRATION = "https://www.pixiv.net/tags/%E3%83%96%E3%83%AB%E3%83%BC%E3%82%A2%E3%83%BC%E3%82%AB%E3%82%A4%E3%83%96/illustrations";
     private static final String IMAGE_BOX_ILLUSTRATION = "#root > div.charcoal-token > div > div:nth-child(4) > div > div > div.sc-15n9ncy-0.jORshO > section > div.sc-l7cibp-0.juyBTC > div:nth-child(1) > ul > li";
+    private static final String DOWNLOAD_PATH = "src/main/resources/static/download/";
 
-    public List<String> getImages() {
+
+    public List<String> downloadImages() {
         ChromeDriver driver = SeleniumConfig.chromeDriver();
         driver.get(URL_ILLUSTRATION);
 
+        autoScroll(driver);
+
+        List<WebElement> imageBox = driver.findElements(By.cssSelector(IMAGE_BOX_ILLUSTRATION));
+        List<String> images = imageBox.stream()
+                .map(o -> o.findElement(By.cssSelector("img.sc-rp5asc-10.erYaF")).getAttribute("src"))
+                .map(o -> {
+                    String fileName = o.substring(o.lastIndexOf('/') + 1);
+                    downloadImageWithReferer(o, DOWNLOAD_PATH + fileName);
+                    return fileName;
+                })
+                .collect(Collectors.toList());
+
+        driver.quit();
+        return images;
+    }
+
+    private static void autoScroll(ChromeDriver driver) {
         Long height = (Long) driver.executeScript("return document.body.scrollHeight");
         Long scroll = 500l;
         while (scroll <= height) {
             driver.executeScript("window.scrollTo(0," + scroll + ")");
             scroll += 500l;
         }
-
-        List<WebElement> imageBox = driver.findElements(By.cssSelector(IMAGE_BOX_ILLUSTRATION));
-        List<String> images = imageBox.stream()
-                .map(o -> o.findElement(By.cssSelector("img.sc-rp5asc-10.erYaF")).getAttribute("src"))
-                .collect(Collectors.toList());
-        List<String> imageNames = new ArrayList<>();
-        for (String imgUrl : images) {
-            String downloadPath = "src/main/resources/static/download/";
-            String fileName = imgUrl.substring(imgUrl.lastIndexOf('/') + 1);
-            downloadImageWithReferer(imgUrl, downloadPath + fileName);
-            imageNames.add(fileName);
-        }
-
-        driver.quit();
-        return imageNames;
     }
 
     private static void downloadImageWithReferer(String imageUrl, String destinationFile) {
@@ -61,7 +65,7 @@ public class CrawlingService {
             // Referer 헤더 설정
             connection.setRequestProperty("Referer", "https://www.pixiv.net/");
 
-            File folder = new File("src/main/resources/static/download/");
+            File folder = new File(DOWNLOAD_PATH);
             if (!folder.exists()) {
                 folder.mkdir();
             }
